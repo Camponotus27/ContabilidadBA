1307d6cd57d447cdd03d75be35039c02a36a1ca4
Sebastian Cornejo <sebastian.cornejo312@gmail.com>
2020-07-26 12:00:37 -0400

    Se agrega la aplicacon sin modificar y se agrega una libreria para manehar .dbf

--- Importador Contable BA/Form1.cs
+++ Importador Contable BA/Form1.cs
@@ -16,5 +16,10 @@ namespace Importador_Contable_BA
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenOfficeApp o = new OpenOfficeApp();
+        }
     }
 }
---
+++ Importador Contable BA/OpenOfficeApp.cs
@@ -0,0 +1,9 @@
+﻿namespace Importador_Contable_BA
+{
+    public class OpenOfficeApp
+    {
+        public OpenOfficeApp()
+        {
+        }
+    }
+}
