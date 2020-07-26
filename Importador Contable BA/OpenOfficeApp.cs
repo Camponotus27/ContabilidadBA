9ac99ebb464bc4fdbe133fc66f4ec9e66c74de23
Sebastian Cornejo <sebastian.cornejo312@gmail.com>
2020-07-26 13:36:09 -0400

    Cambia nombre proyecto, Se crea proyecto de test y se ccrea la primera version de la clase openofficeapp

--- Importador Contable BA/OpenOfficeApp.cs
+++ Importador Contable BA/OpenOfficeApp.cs
@@ -1,9 +1,22 @@
-﻿namespace Importador_Contable_BA
+﻿using dBASE.NET;
+using System;
+
+namespace Importador_Contable_BA
 {
     public class OpenOfficeApp
     {
         public OpenOfficeApp()
         {
+            Dbf dbf = new Dbf();
+            dbf.Read(@"C:\Users\Seba\source\repos\Importador Contable BA\Datos Git\contajvh\APARAM.DBF");
+
+            foreach (DbfRecord record in dbf.Records)
+            {
+                for (int i = 0; i < dbf.Fields.Count; i++)
+                {
+                    Console.WriteLine(record[i]);
+                }
+            }
         }
     }
 }
